7cffb6096518950d1011897cb24a70384a4f587e
Roel <roelhoogendoorn01369@gmail.com>
2024-12-22 18:35:55 +0100

    Improves volume slider

--- Assets/Scripts/Settings/VolumeSlider.cs
+++ Assets/Scripts/Settings/VolumeSlider.cs
@@ -1,18 +1,43 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class VolumeSlider : MonoBehaviour
+public class VolumeSlider : MonoBehaviour, IPointerUpHandler
 {
     public string audioMixerGroup;
-    public Slider volumeSlider;
+    public string sfxOnRelease;
+
+    private Slider volumeSlider;
+    private int lastPlayedValue = 9999;
+
+    private void Awake()
+    {
+        volumeSlider = GetComponent<Slider>();
+
+        if (volumeSlider == null)
+        {
+            Debug.LogError("No Slider component found on this GameObject!");
+        }
+    }
 
     private void Start()
     {
-        volumeSlider.value = SoundManager.Instance.GetVolume(audioMixerGroup);
+        if (volumeSlider != null)
+        {
+            volumeSlider.value = SoundManager.Instance.GetVolume(audioMixerGroup);
+        }
     }
 
     public void ChangeVolume(float volume)
     {
         SoundManager.Instance.SetVolume(audioMixerGroup, volume);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (!string.IsNullOrEmpty(sfxOnRelease))
+        {
+            SoundManager.Instance.PlaySound(audioMixerGroup, sfxOnRelease);
+        }
     }
 }
