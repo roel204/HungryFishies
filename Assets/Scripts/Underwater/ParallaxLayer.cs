ca10c3c180544840d9667496d9ac3fc040fabf42
roel204 <roelhoogendoorn01369@gmail.com>
2025-09-03 11:40:28 +0200

    Update ParallaxLayer.cs

--- Assets/Scripts/Underwater/ParallaxLayer.cs
+++ Assets/Scripts/Underwater/ParallaxLayer.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
-public class ParallaxLayer : MonoBehaviour
-{
+public class ParallaxLayer : MonoBehaviour {
+
     public bool horizontalMovement;
     public float parallaxEffectX;
 
@@ -12,8 +12,7 @@ public class ParallaxLayer : MonoBehaviour
     private Vector2 startPos;
     private Vector2 size;
 
-    void Start()
-    {
+    private void Start() {
         camTransform = Camera.main.transform;
         startPos = transform.position;
         size = new Vector2(
@@ -22,34 +21,25 @@ public class ParallaxLayer : MonoBehaviour
         );
     }
 
-    void FixedUpdate()
-    {
+    private void Update() {
         float distanceX = horizontalMovement ? camTransform.position.x * parallaxEffectX : 0;
         float distanceY = verticalMovement ? camTransform.position.y * parallaxEffectY : 0;
 
         float movementX = horizontalMovement ? camTransform.position.x * (1 - parallaxEffectX) : 0;
         float movementY = verticalMovement ? camTransform.position.y * (1 - parallaxEffectY) : 0;
 
-        if (horizontalMovement)
-        {
-            if (movementX > startPos.x + size.x)
-            {
+        if (horizontalMovement) {
+            if (movementX > startPos.x + size.x) {
                 startPos.x += size.x;
-            }
-            else if (movementX < startPos.x - size.x)
-            {
+            } else if (movementX < startPos.x - size.x) {
                 startPos.x -= size.x;
             }
         }
 
-        if (verticalMovement)
-        {
-            if (movementY > startPos.y + size.y)
-            {
+        if (verticalMovement) {
+            if (movementY > startPos.y + size.y) {
                 startPos.y += size.y;
-            }
-            else if (movementY < startPos.y - size.y)
-            {
+            } else if (movementY < startPos.y - size.y) {
                 startPos.y -= size.y;
             }
         }
